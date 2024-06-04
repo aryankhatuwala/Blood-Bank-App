9facde6cabdf791c86904502ef4c21d061f67b9e
Aryan Khatuwala <aryan.khatuwala@gmail.com>
2024-06-04 13:52:05 +0530

    links added/removed

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
   {
-    origin: ["https://blood-bank-app-dusky.vercel.app"],
+    origin: [""],
     methods: ["POST","GET"],
     credentials: true
   }
